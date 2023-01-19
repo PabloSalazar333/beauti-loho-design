f8fe0b23c83609c75412fad6511046590f117d01
Pablo Esteban Salazar Marquez <pablo.salazar@ibm.com>
2023-01-18 20:49:42 -0600

    Add color on the navbar

---
+++ src/components/pure/forms/CardWidget.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+function CardWidget() {
+  return (
+    <div>
+        <i className="bi bi-cart2 fs-1"></i>
+    </div>
+  )
+}
+
+export default CardWidget
---
+++ src/components/pure/forms/Navbar.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import CardWidget from './CardWidget'
+
+function NavBar() {
+
+  const [clicked, setClicked] = useState(false)
+  const handleClick = () => {
+    //cuando esta true lo pasa a false y vice versa
+    setClicked(!clicked)
+  }
+  return (
+    <>
+      <NavContainer>
+        <h2>Beauty Logo Design</h2>
+        <div className={`links ${clicked ? 'active' : ''}`}>
+          <a href='/'>INICIO</a>
+          <a onClick={handleClick} href='/'>PRODUCTOS</a>
+          <a onClick={handleClick} href='/'>SOBRE MI</a>
+          <a onClick={handleClick} href='/'>CONTACTO</a>
+        </div>
+        <div className='card'>
+          <CardWidget></CardWidget>
+        </div>
+      </NavContainer>
+    </>
+  )
+}
+
+export default NavBar
+
+const NavContainer = styled.nav`
+  h2{
+    color: #fff3fb;
+    font-weight: 400;
+  }
+  padding: .4rem;
+  background-color: #f5bdc6;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  a{
+    color: #fff3fb;
+    text-decoration: none;
+    margin-right: 1rem;
+  }
+
+  .links{
+    position: absolute;
+    top: -700px;
+    left: -2000px;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+    transition: all .5s ease;
+    a{
+      color: white;
+      font-size: 2rem;
+      display: block;
+    }
+    @media(min-width: 768px){
+      position: initial;
+      margin: 0;
+      a{
+        font-size: 1rem;
+        color: white;
+        display: inline;
+      }
+      display: block;
+    }
+  }
+
+  .links.active{
+    width: 100%;
+    display: block;
+    position: absolute;
+    margin-left: auto;
+    margin-right: auto;
+    top: 30%;
+    left: 0;
+    right: 0;
+    text-align: center;
+    a{
+      font-size: 2rem;
+      margin-top: 1rem;
+      color: white;
+    }
+  }
+  .CardWidgetButton{
+    @media(min-width: 768px){
+      display: none;
+    }
+  }
+
+`
